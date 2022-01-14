a3da8c085b1f40a69615047e6937ad14ffd02be6
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-14 16:48:42 +0900

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
 # Add a console message so we can see output when the seed file runs
+
+puts "Seeding games..."
+
+20.times do 
+Game.create(
+  title: Faker::Game.title, 
+  platform: Faker::Game.platform, 
+  genre: Faker::Game.genre, 
+  price: rand(50..60)
+)
+end
+
+puts "Finished seeding!"
